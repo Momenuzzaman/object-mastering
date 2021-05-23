88e3f78f9efcaa47b4c9424780cd90a15c55927f
mdemon420 <mdeamon4@gmail.com>
2021-05-23 20:07:32 +0600

    bind add

--- callApply.js
+++ callApply.js
@@ -1,17 +1,25 @@
 const person ={
     firstName: "Md",
    lastName: "Zaman",
-   salary: 3000,
+   salary: 13000,
    fullName: function(){
-      return(this.firstName,this.lastName);
+      console.log(this.firstName,this.lastName);
    },
    bill: function(amount){
+       console.log(this)
        this.salary = this.salary-amount;
        return this.salary;
    }
-
+};
+const person2 = {
+    firstName: "Rafiq",
+    lastName: "uddin",
+    salary:10000
 }
-console.log(person.firstName,person.lastName);
+// bind
 
-person.bill(1000);
+const person2Bill = person.bill.bind(person2);
+person2Bill(2000);
+person2Bill(2000);
+console.log(person2.salary);
 console.log(person.salary);
