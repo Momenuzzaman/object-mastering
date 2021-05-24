d56bc2f612b512ca1cc3841b25320b230b2a995b
mdemon420 <mdeamon4@gmail.com>
2021-05-24 16:34:30 +0600

    class

---
+++ class.js
@@ -0,0 +1,13 @@
+class Person {
+    constructor(firstName, lastName, salary){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+}
+
+const person1 = new Person("Md", "muktadir", 4000);
+console.log(person1);
+
+const person2 = new Person("Md", "Zaman", 4000);
+console.log(person2);
